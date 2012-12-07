6a6e80208d538d5ebfd4c1e05f1a66c20c25bbc7
Ben Breedlove <benbreedlove@gmail.com>
2012-12-06 16:07:00 -0800

    first commit

---
+++ src/GDoc-Powered-Quiz.js
@@ -0,0 +1,28 @@
+/*
+ * GDoc-Powered-Quiz
+ * https://github.com/motherjones/GDoc-Powered-Quiz
+ *
+ * Copyright (c) 2012 Ben Breedlove
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
