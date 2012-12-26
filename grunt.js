f45b4fa7d62e8b81c4649d92bf07dad6627bd452
Ben Breedlove <benbreedlove@gmail.com>
2012-12-26 15:51:50 -0800

    change where we store our dist files

--- grunt.js
+++ grunt.js
@@ -14,13 +14,13 @@ module.exports = function(grunt) {
     concat: {
       dist: {
         src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
+        dest: 'demo/js/<%= pkg.name %>.js'
       }
     },
     min: {
       dist: {
         src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+        dest: 'demo/js/<%= pkg.name %>.min.js'
       }
     },
     qunit: {
