a0a37e81f65bad9a1b1609364a5dcbe8ebcdbf70
Casey Thomas <cpthomas@gmail.com>
2013-04-26 14:30:03 -0400

    Fix banner comment formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       options: {
         stripBanners: true,
         banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %> ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> \n' +
         '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
